d06be3b4f2e5f1561225b11670f0233f162266bd
Administrator <Administrator@PC-20171115ONGG>
2019-08-27 15:28:34 +0800

    V0.01

---
+++ sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.tedu.sp01.service;
+
+import java.util.List;
+
+import com.tedu.sp01.pojo.Item;
+
+public interface ItemService {
+	/**
+	 * 通过订单id来获取商品信息
+	 */
+	List<Item> getItems(String orderId);
+	void decreaseNumbers(List<Item> list);
+}
